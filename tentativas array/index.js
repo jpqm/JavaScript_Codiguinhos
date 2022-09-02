35d70780c7148c1d29830b34dd1ad2fa182f1289
jpqm <36828739+jpqm@users.noreply.github.com>
2022-09-02 14:31:42 -0300

    Update index.js

--- tentativas array/index.js
+++ tentativas array/index.js
@@ -3,8 +3,10 @@ let tent = 0;
 let achou = false;
 let array2 = [];
 
+//inicia cronometro
 const inicio = performance.now();
 
+//gera vetor aleatorio tamanho 10
 for(let k = 0; k < 10; k+=1){
 
     let x = Math.floor(Math.random()*5 +1);
@@ -19,11 +21,13 @@ console.log(...array);
 while(!achou) {
     tentativas(i);
     let iguais=0;
+    //confere se os valores do novo vetor gerado
     for(let k = 0; k < i; k+=1){
         if(array2[k] == array[k]){
         iguais++
     }
 }
+    //caso ache segundo vetor igual ao primeiro acaba o loop
     if(iguais==10)
         achou=true;
 
@@ -33,14 +37,17 @@ while(!achou) {
 console.log(...array2);
 console.log(tent + " tentativas");
 
+//finaliza cronometro
 const fim = performance.now();
 
+//mostra na tela vetor1 vetor2 n° de tentativas e tempo em segundos
 document.getElementById("array1"). innerHTML = array;
 document.getElementById("array2"). innerHTML = array2;
 document.getElementById("tentativas"). innerHTML = tent.toLocaleString("pt-BR") + " tentativas";
 
 document.getElementById("tempo"). innerHTML = `${(fim - inicio)/1000} segundos`;
 
+//gera vetor aleatorio novo
 function tentativas(j){
     for(let k = 0; k < j; k+=1){
         let x = Math.floor(Math.random()*5 +1);
